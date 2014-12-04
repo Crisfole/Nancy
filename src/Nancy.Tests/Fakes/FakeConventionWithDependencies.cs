d7d469219ee1e5d4a2818bddd697725a0572c977
Christopher Pfohl <Christopher.Pfohl@gmail.com>
2014-12-04 09:51:01 -0500

    Add class that makes tests fail

---
+++ src/Nancy.Tests/Fakes/FakeConventionWithDependencies.cs
@@ -0,0 +1,22 @@
+﻿namespace Nancy.Tests.Fakes
+{
+    using System;
+    using Nancy.Conventions;
+
+    /// <summary>
+    /// A do-nothing IConvention that demonstrates a bug in resolving conventions
+    /// </summary>
+    public class FakeConventionWithDependencies : IConvention {
+        private Person dependantUpon;
+        public FakeConventionWithDependencies(Person dependantUpon) {
+            this.dependantUpon = dependantUpon;
+        }
+
+        public void Initialise(NancyConventions conventions) {
+        }
+
+        public Tuple<bool, string> Validate(NancyConventions conventions) {
+            return Tuple.Create(true, (string)null);
+        }
+    }
+}
